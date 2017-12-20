cdd12bdbea0e0aa5cef1955ad2d7a03c9b98b044
1STG\osawa.y <osawa.yoshiki@gmail.com>
2017-12-20 19:31:41 +0900

    first commit.

--- 1_motor.ino
+++ 1_motor.ino
@@ -1,21 +1,21 @@
-#define PIN1 5
-#define PIN2 6
-#define PWM1 7
+#define M_PIN1 5
+#define M_PIN2 6
+#define M_PWM1 7
 
 void setup() {
   Serial.begin(9600);
   Serial.println("hello, arduino!");
-  pinMode(PIN1, OUTPUT);
-  pinMode(PIN2, OUTPUT);
-  pinMode(PWM1, OUTPUT);
+  pinMode(M_PIN1, OUTPUT);
+  pinMode(M_PIN2, OUTPUT);
+  pinMode(M_PWM1, OUTPUT);
 }
 
 void loop() {
-  digitalWrite(PIN1, HIGH);
-  digitalWrite(PIN2, LOW);
-  analogWrite(PWM1, 200);
+  digitalWrite(M_PIN1, HIGH);
+  digitalWrite(M_PIN2, LOW);
+  analogWrite(M_PWM1, 200);
 
   delay(1000);
-  analogWrite(PWM1, 0);
+  analogWrite(M_PWM1, 0);
   delay(1000);
 }
